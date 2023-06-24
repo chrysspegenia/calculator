ebe38012f0dd284540d1578e0df7eac3a07c1d37
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2023-06-24 19:41:27 +0800

    Modify eventListener to include buttonsContainer

--- style-effects.js
+++ style-effects.js
@@ -1,14 +1,27 @@
-const handleOnMouseMove = e => {
-    const {currentTarget: target} = e;
+// const handleOnMouseMove = e => {
+//     const {currentTarget: target} = e;
 
-    const rect = target.getBoundingClientRect(),
+//     const rect = target.getBoundingClientRect(),
+//         x = e.clientX - rect.left,
+//         y = e.clientY - rect.top;
+
+//     target.style.setProperty("--mouse-x", `${x}px`);
+//     target.style.setProperty("--mouse-y", `${y}px`);
+// }
+
+// for(const card of document.querySelectorAll(".button")){
+//     card.onmousemove = e => handleOnMouseMove(e);
+// }
+
+
+document.getElementById("buttonsContainer").onmousemove = e => {
+    for(const button of document.getElementsByClassName("button")){
+        
+        const rect = button.getBoundingClientRect(),
         x = e.clientX - rect.left,
         y = e.clientY - rect.top;
 
-    target.style.setProperty("--mouse-x", `${x}px`);
-    target.style.setProperty("--mouse-y", `${y}px`);
-}
-
-for(const card of document.querySelectorAll(".button")){
-    card.onmousemove = e => handleOnMouseMove(e);
+        button.style.setProperty("--mouse-x", `${x}px`);
+        button.style.setProperty("--mouse-y", `${y}px`);
+    }
 }
