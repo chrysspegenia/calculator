cec1f8abb91619846016a17f0d80269adaaac527
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2023-06-28 16:41:26 +0800

    Create add, subtract, multiply and divide functions

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+
+function add(firstNum, secondNum){
+    return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum){
+    return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum){
+    return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum){
+    return firstNum / secondNum;
+}
