e6ca2322ca66323d8746cfe1a021b661c09faa21
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2023-06-29 09:50:28 +0800

    Add operate function

--- index.js
+++ index.js
@@ -19,3 +19,12 @@ function multiply(firstNum, secondNum){
 function divide(firstNum, secondNum){
     return firstNum / secondNum;
 }
+
+//keep option to replace value to actual symbols for HTML text display
+function operate(firstNum, secondNum, operator){
+    if(operator === "add") return add(firstNum, secondNum);
+    if(operator ==="subtract") return subtract(firstNum, secondNum);
+    if(operator === "multiply") return multiply(firstNum, secondNum);
+    if(operator ==="divide") return divide(firstNum, secondNum);
+}
+console.log(operate(firstNum, secondNum, operator))
